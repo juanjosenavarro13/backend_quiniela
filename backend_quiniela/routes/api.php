41f942d773059bbb9814b33389b6f84b81d13546
juanxo13597 <juanxo97master@gmail.com>
2022-10-07 18:15:45 +0200

    init with jwt

---
+++ backend_quiniela/routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::group(['prefix' => 'v1'], function ($router) {
+    // auth
+    Route::group([
+        'middleware' => 'api',
+        'namespace' => 'App\Http\Controllers',
+        'prefix' => 'auth'
+    ], function ($router) {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+        // Route::post('register', [AuthController::class, 'register']);
+    });
+});
+
+// route sin permisos
+route::get('login', function () {
+    return response()->json('Usuario sin permisos', 400);
+})->name('login');
